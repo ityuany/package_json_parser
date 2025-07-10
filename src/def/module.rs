7f95f5d0e4da3405e85f27d68d5dfe7723b42fa1
ityuany <519495771@qq.com>
2025-07-10 16:14:41 +0800

    重构 Module 结构体，将其从传统字段形式修改为新形式，使用 Deref 和 DerefMut 特性以增强灵活性和可读性，同时注释掉旧的实现。

--- src/def/module.rs
+++ src/def/module.rs
@@ -1,13 +1,17 @@
+use derive_more::{Deref, DerefMut};
 use jsonc_parser::ast::ObjectProp;
 use serde::{Deserialize, Serialize};
 
 use crate::ext::Validator;
 
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct Module {
-  pub r#type: String,
-  pub value: String,
-}
+// #[derive(Debug, Serialize, Deserialize, Clone)]
+// pub struct Module {
+//   pub r#type: String,
+//   pub value: String,
+// }
+
+#[derive(Debug, Serialize, Deserialize, Clone, Deref, DerefMut)]
+pub struct Module(String);
 
 impl Validator for Module {
   fn validate(&self, _prop: Option<&ObjectProp>) -> miette::Result<()> {
